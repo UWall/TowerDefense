2bc297c72e0659d20b591316fb7406d4877adbc5
Nirvana O. B <facnirv@gmail.com>
2022-10-15 22:55:01 -0300

    Configurando

--- Assets/Scripts/MixerController.cs
+++ Assets/Scripts/MixerController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MixerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer mixer;
+    public Slider musicValue;
+    public Slider efeitosValue;
+    public Slider masterVol;
+
+
+    private void Start()
     {
-        
+        PlayerPrefs.GetFloat("masterVolparameter", masterVol.value);
+        PlayerPrefs.GetFloat("musicVolparameter", musicValue.value);
+        PlayerPrefs.GetFloat("efeitosVolparameter", efeitosValue.value);
+    }
+    public void MasterVolumChange()
+    {
+        mixer.SetFloat("masterVolparameter", masterVol.value);
+        PlayerPrefs.SetFloat("masterVolparameter", masterVol.value);
+        PlayerPrefs.Save();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MusicVolumChange()
     {
-        
+        mixer.SetFloat("musicVolparameter", musicValue.value);
+        PlayerPrefs.SetFloat("musicVolparameter", musicValue.value);
+    }
+    public void EfeitosVolumChange()
+    {
+        mixer.SetFloat("efeitosVolparameter", efeitosValue.value);
+        PlayerPrefs.SetFloat("efeitosVolparameter", efeitosValue.value);
     }
 }
+
